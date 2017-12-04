9f4d24dc054b8bf0839df85040d812925d83ed15
Edsel <edsel.lopez@jmaconsulting.biz>
2017-12-04 17:58:03 +0530

    YEEHONG-38 Added weekly report

---
+++ CRM/Yeehongreports/Form/Report/WeeklyReport.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Yeehongreports_Form_Report_WeeklyReport',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'Weekly Mailing Report',
+      'description' => 'Weekly Mailing Report (biz.jmaconsulting.yeehongreports)',
+      'class_name' => 'CRM_Yeehongreports_Form_Report_WeeklyReport',
+      'report_url' => 'weeklyreport',
+      'component' => 'CiviMail',
+    ),
+  ),
+);
