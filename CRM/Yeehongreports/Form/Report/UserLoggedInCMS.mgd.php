e154253bc75590fb4e7f51c152bcb751cbe4012f
Edsel <edsel.lopez@jmaconsulting.biz>
2017-12-04 19:17:46 +0530

    YEEHONG-38 Added user logged in report

---
+++ CRM/Yeehongreports/Form/Report/UserLoggedInCMS.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Yeehongreports_Form_Report_UserLoggedInCMS',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'UserLoggedInCMS',
+      'description' => 'UserLoggedInCMS (biz.jmaconsulting.yeehongreports)',
+      'class_name' => 'CRM_Yeehongreports_Form_Report_UserLoggedInCMS',
+      'report_url' => 'userlogin',
+      'component' => 'CiviMail',
+    ),
+  ),
+);
